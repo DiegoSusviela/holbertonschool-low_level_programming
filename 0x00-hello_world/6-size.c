dece7d971c1a7b88a0397536fae370ce88a93f0b
DiegoSusviela <dieguitosus@hotmail.com>
2021-02-03 17:01:00 +0000

    fix 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,9 +1,16 @@
 #include <stdio.h>
-int main(){
-  printf("Size of a char: %zu byte(s) \n", sizeof(int));
-  printf("Size of an int: %zu byte(s) \n", sizeof(long));
-  printf("Size of a long int: %zu byte(s) \n", sizeof(int));
-  printf("Size of a long long int: %zu byte(s) \n", sizeof(long));
-  printf("Size of a float: %zubyte (s) \n", sizeof(int));
-return 0;
+/**
+ * main - Entry point
+ *
+ * Description: Show a message blablabla
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+printf("Size of a char: %zu byte(s) \n", sizeof(char));
+printf("Size of an int: %zu byte(s) \n", sizeof(int));
+printf("Size of a long int: %zu byte(s) \n", sizeof(long int));
+printf("Size of a long long int: %zu byte(s) \n", sizeof(long long int));
+printf("Size of a float: %zubyte (s) \n", sizeof(float));
+return (0);
 }
