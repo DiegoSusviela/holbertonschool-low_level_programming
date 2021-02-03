f9212ba565c361ce85b72f14ada8e343e621fc0f
DiegoSusviela <dieguitosus@hotmail.com>
2021-02-03 16:37:35 +0000

    more betty

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 /*
-wopa
+ *wopa
  */
 
 int main(void)
 {
-  puts("\"Programming is like building a multilingual puzzle");
-  return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
