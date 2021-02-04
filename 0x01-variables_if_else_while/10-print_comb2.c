6a07235b0da22348d494b81cc9af5b83c589688a
DiegoSusviela <dieguitosus@hotmail.com>
2021-02-04 18:36:28 +0000

    rancio10

--- 0x01-variables_if_else_while/10-print_comb2.c
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -15,16 +15,19 @@ int main(void)
 	int x, w;
 
 	for (w = '0'; w <= '9'; w++)
-		     for (x = '0'; x <= '9'; x++)
-		     {
-			     putchar(w);
-			     putchar(x);
-			     if (!(x == '9' && w == '9'))
-			     {
-				     putchar(',');
-				     putchar(' ');
-			     }
-		     }
+		for (x = '0'; x <= '9'; x++)
+		{
+			putchar(w);
+			putchar(x);
+			if (x == '9' && w == '9')
+			{
+			}
+			else
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
 	putchar('\n');
 	return (0);
 }
