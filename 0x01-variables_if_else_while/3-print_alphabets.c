0623209ce795316f4712b7a1e9c5a4eb536321b8
DiegoSusviela <dieguitosus@hotmail.com>
2021-02-04 16:57:40 +0000

    fix3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -12,13 +12,10 @@
 
 int main(void)
 {
-	int w, x;
+	char x;
 
-	for (x = 'A'; x <= 'Z'; x++)
-	{
-		w = tolower(x);
-		putchar(w);
-	}
+	for (x = 'a'; x <= 'z'; x++)
+		putchar(x);
 	for (x = 'A'; x <= 'Z'; x++)
 		putchar(x);
 	putchar('\n');
