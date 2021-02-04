6d15433c674e8b920ab330909f7566ba2cfd360f
DiegoSusviela <dieguitosus@hotmail.com>
2021-02-04 21:17:28 +0000

    sera?

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -12,33 +12,33 @@
 
 int main(void)
 {
-int d1, d2, u1, u2, i;
-  
-for (d1 = '0'; d1 <= '9'; d1++)
-for (u1 = '0'; u1 <= '8'; u1++)
-for (d2 = d1; d2 <= '9'; d2++)
-{
-if (d1 == d2)
-i = u1 + 1;
-else 
-i = '0';
-for (u2 = i; u <= '9'; u2++)
-{
-putchar(d1);
-putchar(u1);
-putchar(' ');
-putchar(d2);
-putchar(u2);
-if (d1 == '9' && u1 == '8' && d1 == '9' && u2 == '9')
-{
-}
-else
-{
-putchar(',');
-putchar(' ');
-}
-}
-}
-putchar('\n');
-return (0);
+	int d1, d2, u1, u2, i;
+
+	for (d1 = '0'; d1 <= '9'; d1++)
+		for (u1 = '0'; u1 <= '8'; u1++)
+			for (d2 = d1; d2 <= '9'; d2++)
+			{
+				if (d1 == d2)
+					i = u1 + 1;
+				else
+					i = '0';
+				for (u2 = i; u2 <= '9'; u2++)
+				{
+					putchar(d1);
+					putchar(u1);
+					putchar(' ');
+					putchar(d2);
+					putchar(u2);
+					if (d1 == '9' && u1 == '8' && d1 == '9' && u2 == '9')
+					{
+					}
+					else
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+		}
+	putchar('\n');
+	return (0);
 }
