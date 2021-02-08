7893249b18c92e310fb412ca84785c55fff2fe43
DiegoSusviela <dieguitosus@hotmail.com>
2021-02-08 19:39:55 +0000

    added6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * _abs - Entry point
+ *
+ * Description: Show a message blablabla
+ * Return: Always 0 (Success)
+ */
+
+int _abs(int x)
+{
+	if (x >= 0)
+		return (x);
+		else
+			return (-x);
+
+}
