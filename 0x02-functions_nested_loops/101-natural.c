751a23cf4ea049bc56b4ab5471c3fdc0bdbce54d
DiegoSusviela <dieguitosus@hotmail.com>
2021-02-09 17:48:43 +0000

    new101

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,34 +1,11 @@
 #include "holberton.h"
 
 /**
- * print_num - imprime
- * @num: numero a imprimir
- *
- * Description: Show a message blablabla
- * Return: Always 0 (Success)
- */
-
-void print_num(int num)
-{
-	int resto;
-
-	resto = num % 10;
-	if (num >= 10)
-	{
-		num = num / 10;
-		print_num(num);
-	}
-	_putchar('0' + resto);
-}
-
-
-/**
  * main - imprime
  *
  * Description: Show a message blablabla
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
 	int suma, contador;
@@ -36,7 +13,7 @@ int main(void)
 	for (contador = 1023; contador >= 1; contador--)
 		if (((contador % 3) == 0) || ((contador % 5) == 0))
 			suma = suma + contador;
-	print_num(suma);
-	_putchar('\n');
+	printf("%d", suma);
+	_putchar("\n");
 	return (suma);
 }
