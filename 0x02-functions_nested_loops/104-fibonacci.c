9f073b169b48a24138a5b7f736d9bec1b1de7e52
DiegoSusviela <dieguitosus@hotmail.com>
2021-02-09 22:44:15 +0000

    problemasconlong

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+ * main - imprime
+ *
+ * Description: Show a message blablabla
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	long int i, t1 = 1, t2 = 2, nextTerm;
+
+	for (i = 1; i <= 98; ++i)
+	{
+		if (i != 98)
+			printf("%ld, ", t1);
+		else
+			printf("%ld", t1);
+		nextTerm = t1 + t2;
+		t1 = t2;
+		t2 = nextTerm;
+	}
+	printf("\n");
+	return (0);
+}
