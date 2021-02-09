c6079d67fa6623c252d095005b83a89b97b733bc
DiegoSusviela <dieguitosus@hotmail.com>
2021-02-09 14:27:52 +0000

    added100

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,6 +1,84 @@
 #include "holberton.h"
 
 /**
+ * print_n_low - Entry point
+ *
+ * Description: Show a message blablabla
+ * Return: Always 0 (Success)
+ */
+
+void print_n_low(void)
+{
+	if (y != n)
+	{
+		if (y != 0)
+		{
+			_putchar(' ');
+			_putchar(' ');
+		}
+		_putchar('0' + comb);
+		_putchar(',');
+		_putchar(' ');
+	}
+	else
+	{
+		_putchar(' ');
+		_putchar(' ');
+		_putchar('0' + comb);
+		_putchar('\n');
+	}
+}
+
+/**
+ * print_n_med - Entry point
+ *
+ * Description: Show a message blablabla
+ * Return: Always 0 (Success)
+ */
+
+void print_n_med(void)
+{
+	_putchar(' ');
+	uni = comb % 10;
+	dec = comb / 10;
+	_putchar('0' + dec);
+	_putchar('0' + uni);
+	if (y != n)
+	{
+		_putchar(',');
+		_putchar(' ');
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
+/**
+ * print_n_high - Entry point
+ *
+ * Description: Show a message blablabla
+ * Return: Always 0 (Success)
+ */
+
+void print_n_high(void)
+{
+	uni = comb % 10;
+	dec = (comb / 10) % 10;
+	cen = comb / 100;
+	_putchar('0' + cen);
+	_putchar('0' + dec);
+	_putchar('0' + uni);
+	if (y != n)
+	{
+		_putchar(',');
+		_putchar(' ');
+	}
+	else
+		_putchar('\n');
+}
+
+
+/**
  * print_times_table - Entry point
  *@n: wopa
  *
@@ -8,72 +86,21 @@
  * Return: Always 0 (Success)
  */
 
-
 void print_times_table(int n)
 {
-  int y, x, comb, uni, dec, cen;
+	int y, x, comb, uni, dec, cen;
 
-  if (n <= 15 && n >= 0)
-    for (x = 0; x <= n; x++)
-      for (y = 0; y <= n; y++)
-	{
-	  comb = x * y;
-	  if (comb < 10)
-	    {
-	      if (y != n)
-		{
-		  if (y != 0)
-		    {
-		      _putchar(' ');
-		      _putchar(' ');
-		    }
-		  _putchar('0' + comb);
-		  _putchar(',');
-		  _putchar(' ');
-		}
-	      else
-		{
-		  _putchar(' ');
-		  _putchar(' ');
-		  _putchar('0' + comb);
-		  _putchar('\n');
-		}
-	    }
-	  else
-	    if (comb < 100)
-	      {
-		_putchar(' ');
-		uni = comb % 10;
-		dec = comb / 10;
-		_putchar('0' + dec);
-		_putchar('0' + uni);
-		if (y != n)
-		  {
-		    _putchar(',');
-		    _putchar(' ');
-		  }
-		else
-		  {
-		    _putchar('\n');
-		  }
-	      }
-	    else
-	      {
-		uni = comb % 10;
-		dec = (comb / 10) % 10;
-		cen = comb / 100;
-		_putchar('0' + cen);
-		_putchar('0' + dec);
-		_putchar('0' + uni);
-		if (y != n)
-		  {
-		    _putchar(',');
-		    _putchar(' ');
-		  }
-		else
-		  {
-		    _putchar('\n');
-		  }
-	      }
-	}
+	if (n <= 15 && n >= 0)
+		for (x = 0; x <= n; x++)
+			for (y = 0; y <= n; y++)
+			{
+				comb = x * y;
+				if (comb < 10)
+					print_n_low();
+				else
+					if (comb < 100)
+						print_n_med();
+					else
+						print_n_high();
+			}
 }
