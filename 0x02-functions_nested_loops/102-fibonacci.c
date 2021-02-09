1b0a0dcb3fdc688747f7cbca9f2e4bd242625635
DiegoSusviela <dieguitosus@hotmail.com>
2021-02-09 21:50:22 +0000

    addedfibo

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -7,12 +7,16 @@
  * Return: Always 0 (Success)
  */
 
-int main() {
+int main(void)
+{
 	int i, t1 = 1, t2 = 2, nextTerm;
 
 	for (i = 1; i <= 50; ++i)
 	{
-		printf("%d, ", t1);
+		if (i != 50)
+			printf("%d, ", t1);
+		else
+			printf("%d", t1);
 		nextTerm = t1 + t2;
 		t1 = t2;
 		t2 = nextTerm;
