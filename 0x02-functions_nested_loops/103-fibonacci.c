deb1a1086b1df4954e334797eb2e9f674266b296
DiegoSusviela <dieguitosus@hotmail.com>
2021-02-09 22:11:41 +0000

    added2fibo

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+
+/**
+ * main - imprime
+ *
+ * Description: Show a message blablabla
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	long int i, t1 = 1, t2 = 2, nextTerm;
+
+	for (i = 1; i <= 32; ++i)
+	{
+		if (t1 % 2 == 0)
+		{
+			if (i != 32)
+				printf("%ld, ", t1);
+			else
+				printf("%ld", t1);
+		}
+		nextTerm = t1 + t2;
+		t1 = t2;
+		t2 = nextTerm;
+
+	}
+	printf("\n");
+	return (0);
+}
