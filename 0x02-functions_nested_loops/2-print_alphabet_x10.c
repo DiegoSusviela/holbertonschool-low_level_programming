1cc1f51ab743606f403253c6bcf841578cbaca14
DiegoSusviela <dieguitosus@hotmail.com>
2021-02-09 14:11:44 +0000

    fix2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -13,9 +13,5 @@ void print_alphabet_x10(void)
 	int i;
 
 	for (i = 0 ; i <= 9; i++)
-		for (letter = 'a'; letter <= 'z'; letter++)
-		{
-			_putchar(letter);
-			_putchar('\n');
-		}
+		print_alphabet();
 }
