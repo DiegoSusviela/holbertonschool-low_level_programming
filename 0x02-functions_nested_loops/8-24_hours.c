8c2997f54c3a8910d25056835f8882abba7add83
DiegoSusviela <dieguitosus@hotmail.com>
2021-02-09 14:00:34 +0000

    bettylikes8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -9,23 +9,23 @@
 
 void jack_bauer(void)
 {
-  int hour1, hour2, min1, min2, tope;
-  for (hour1 = 0; hour1 <= 2; hour1++)
-    {
-      if (hour1 == 2)
-	tope = 4;
-      else
-	tope = 9;
-      for (hour2 = 0; hour2 <= tope; hour2++)
-	for (min1 = 0; min1 <= 5; min1++)
-	  for (min2 = 0; min2 <= 9; min2++)
-	    {
-	      _putchar('0' + hour1);
-	      _putchar('0' + hour2);
-	      _putchar(':');
-	      _putchar('0' + min1);
-	      _putchar('0' + min2);
-	      _putchar('\n');
-	    }
-    }
+	int hour1, hour2, min1, min2, tope;
+	for (hour1 = 0; hour1 <= 2; hour1++)
+	{
+		if (hour1 == 2)
+			tope = 3;
+		else
+			tope = 9;
+		for (hour2 = 0; hour2 <= tope; hour2++)
+			for (min1 = 0; min1 <= 5; min1++)
+				for (min2 = 0; min2 <= 9; min2++)
+				{
+					_putchar('0' + hour1);
+					_putchar('0' + hour2);
+					_putchar(':');
+					_putchar('0' + min1);
+					_putchar('0' + min2);
+					_putchar('\n');
+				}
+	}
 }
