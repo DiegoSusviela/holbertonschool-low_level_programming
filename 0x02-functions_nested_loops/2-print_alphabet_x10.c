ad023d7c9d4fa2d347164be7e71c9ca5b4f00859
DiegoSusviela <dieguitosus@hotmail.com>
2021-02-09 14:09:38 +0000

    bnettylikes2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -11,7 +11,11 @@
 void print_alphabet_x10(void)
 {
 	int i;
-				      
+
 	for (i = 0 ; i <= 9; i++)
-		print_alphabet();
+		for (letter = 'a'; letter <= 'z'; letter++)
+		{
+			_putchar(letter);
+			_putchar('\n');
+		}
 }
