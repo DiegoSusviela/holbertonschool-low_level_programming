c3050efa6255131fedfdd2d8436044c09a2cebf4
DiegoSusviela <dieguitosus@hotmail.com>
2021-02-11 16:22:34 +0000

    cases

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -14,10 +14,11 @@ void print_diagonal(int n)
 
 	for (i = 1; i <= n; i++)
 	{
-		for (j = 1; j <= i; i++)
+		for (j = 1; j <= i; j++)
 			_putchar(' ');
 		_putchar('\\');
 		_putchar('\n');
 	}
-	_putchar('\n');
+	if (n <= 0)
+		_putchar('\n');
 }
