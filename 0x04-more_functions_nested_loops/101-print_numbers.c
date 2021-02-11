40c935ba60321e62ad91b31af52a6d19fdfe9467
DiegoSusviela <dieguitosus@hotmail.com>
2021-02-11 18:07:09 +0000

    wopa

--- 0x04-more_functions_nested_loops/101-print_numbers.c
+++ 0x04-more_functions_nested_loops/101-print_numbers.c
@@ -8,34 +8,22 @@
  * Return: Always 0 (Success)
  */
 
-void imprimir(unsigned int num)
+void print_number(int n)
 {
+	unsigned int num;
 	int resto;
 
-	resto = num % 10;
+	if (n < 0)
+	{
+		_putchar('-');
+		num = -n;
+	}
+	else
+		num = n;
 	if (num >= 10)
 	{
 		num = num / 10;
 		imprimir(num);
 	}
 	_putchar('0' + resto);
-}
-
-/**
- * print_number - Entry point
- * @n: wopa
- *
- * Description: Show a message blablabla
- * Return: Always 0 (Success)
- */
-
-void print_number(int n)
-{
-	if (n >= 0)
-		imprimir(n);
-	else
-	{
-		_putchar('-');
-		imprimir(-n);
-	}
 }
