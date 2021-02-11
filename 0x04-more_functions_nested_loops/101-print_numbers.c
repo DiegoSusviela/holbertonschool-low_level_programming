a70505225c9c2c2a04f756b7d7a139f9ba8ae323
DiegoSusviela <dieguitosus@hotmail.com>
2021-02-11 17:57:28 +0000

    wopa

---
+++ 0x04-more_functions_nested_loops/101-print_numbers.c
@@ -0,0 +1,38 @@
+#rimir - Entry point
+* @num: wopa
+ *
+* Description: Show a message blablabla
+* Return: Always 0 (Success)
+ */
+
+void imprimir(unsigned int num)
+{
+	int resto;
+
+	resto = num % 10;
+	if (num >= 10)
+	{
+		num = num / 10;
+		imprimir(num);
+	}
+	_putchar('0' + resto);
+}
+
+/**
+ * print_number - Entry point
+ * @n: wopa
+ *
+ * Description: Show a message blablabla
+ * Return: Always 0 (Success)
+ */
+
+void print_number(int n)
+{
+	if (n >= 0)
+		imprimir(n);
+	else
+	{
+		_putchar('-');
+		imprimir(-n);
+	}
+}
