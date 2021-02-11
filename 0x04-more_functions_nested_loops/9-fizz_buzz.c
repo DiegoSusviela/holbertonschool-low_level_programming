aa266edb29f1525ddf88be0b4ac7c64c681903f2
DiegoSusviela <dieguitosus@hotmail.com>
2021-02-11 16:00:04 +0000

    removedspace

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,7 +11,7 @@ int main(void)
 {
 	int i;
 
-	for (i = 1; i <= 100; i++)
+	for (i = 1; i < 100; i++)
 		if (i % 3 == 0)
 			printf("Fizz ");
 		else
@@ -19,6 +19,6 @@ int main(void)
 				printf("Buzz ");
 			else
 				printf("%d ", i);
-	printf("\n");
+	printf("Buzz\n");
 	return (0);
 }
