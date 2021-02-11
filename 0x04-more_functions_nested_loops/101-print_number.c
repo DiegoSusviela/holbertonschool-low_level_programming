45768c9ac1a393f5777eb5b407a9324c841f7797
DiegoSusviela <dieguitosus@hotmail.com>
2021-02-11 18:14:15 +0000

    wopa

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,27 +1,6 @@
 #include "holberton.h"
 
 /**
- * imprimir - Entry point
- * @num: wopa
- *
- * Description: Show a message blablabla
- * Return: Always 0 (Success)
- */
-
-void imprimir(int num)
-{
-	int resto;
-
-	resto = num % 10;
-	if (num >= 10)
-	{
-		num = num / 10;
-		imprimir(num);
-	}
-	_putchar('0' + resto);
-}
-
-/**
  * print_number - Entry point
  * @n: wopa
  *
@@ -31,11 +10,19 @@ void imprimir(int num)
 
 void print_number(int n)
 {
-	if (n >= 0)
-		imprimir(n);
-	else
+	unsigned int num;
+
+	if (n < 0)
 	{
 		_putchar('-');
-		imprimir(-n);
+		num = -n;
 	}
+	else
+		num = n;
+	if (num / 10 != 0)
+	{
+		num = num / 10;
+		print_number(num);
+	}
+	_putchar('0' + num);
 }
