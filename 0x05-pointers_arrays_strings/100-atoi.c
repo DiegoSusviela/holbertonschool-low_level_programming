948ac9810088e5665a002ba7a43b514dc8869ff7
DiegoSusviela <dieguitosus@hotmail.com>
2021-02-16 14:28:27 -0300

    added function to check conditions

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,7 +1,23 @@
 #include "holberton.h"
 
 /**
- * _strlen - Entry point
+ * get_first - Entry point
+ * @s: wopa
+ * @finder1: wopa
+ * Description: Show a message blablabla
+ * Return: Always 0 (Success)
+ */
+
+int get_first(char *s, int finder1)
+{
+    if (*(s + finder1) > '9' || *(s + finder1) < '0')
+        return (1);
+    else
+        return (0);
+}
+
+/**
+ * _atoi - Entry point
  * @s: wopa
  *
  * Description: Show a message blablabla
@@ -12,7 +28,7 @@ int _atoi(char *s)
 {
 	int finder1 = 0, num = 0, sign = 1;
 
-	while ((*(s + finder1) > '9' || *(s + finder1) < '0') && (*(s + finder1) != '\0'))
+	while ((get_first(s, finder1) == 1) && (*(s + finder1) != '\0'))
 	{
 		if (*(s + finder1) == '-')
 			sign *= -1;
