c9e7030150b3f4e9ba6242aa2806aa85cc52d90a
DiegoSusviela <dieguitosus@hotmail.com>
2021-02-17 20:22:57 -0300

    tidy code

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -11,22 +11,22 @@
 
 int main(void)
 {
-	int Pos_pw, random_pos, Sum_cond, filling;
+	int pos_pw, random_pos, sum_cond, filling;
 	char carac_posib[] = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"; 
 	char pw[58];
 
-    	srand(time(NULL));
-		Pos_pw = Sum_cond = filling = 0;
-		while ((2650) > Sum_cond)
-		{
-			random_pos = rand() % 62;
-			pw[Pos_pw] = carac_posib[random_pos];
-			Sum_cond = Sum_cond + carac_posib[random_pos];
-			Pos_pw++;
-		}		        
-	pw[Pos_pw] = 2772 - Sum_cond;
-	Pos_pw++;
-	pw[Pos_pw] = '\0';
+    srand(time(NULL));
+	pos_pw = sum_cond = filling = 0;
+	while ((2650) > sum_cond)
+	{
+		random_pos = rand() % 62;
+		pw[pos_pw] = carac_posib[random_pos];
+		sum_cond = sum_cond + carac_posib[random_pos];
+		pos_pw++;
+	}		        
+	pw[pos_pw] = 2772 - sum_cond;
+	pos_pw++;
+	pw[pos_pw] = '\0';
 	printf("%s", pw);
 	return (0);
 }
