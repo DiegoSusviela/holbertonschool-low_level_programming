992b81733a5baf3953f15ceb748d12f259c2e280
DiegoSusviela <dieguitosus@hotmail.com>
2021-02-17 19:29:41 -0300

    sera esta?

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,41 +1,45 @@
-#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 /**
- * get_first - Entry point
- * @s: wopa
- * @finder1: wopa
- * Description: Show a message blablabla
- * Return: Always 0 (Success)
- */
-int get_first(char *s, int finder1)
-{
-if (*(s + finder1) > '9' || *(s + finder1) < '0')
-return (1);
-else
-return (0);
-}
-
-/**
- * _atoi - Entry point
- * @s: wopaa
+ * main - Entry point
  *
  * Description: Show a message blablabla
  * Return: Always 0 (Success)
  */
-int _atoi(char *s)
-{
-	int finder1 = 0, num = 0, sign = 1;
 
-	while ((get_first(s, finder1) == 1) && (*(s + finder1) != '\0'))
+int main(void)
+{
+	int Pos_pw, random_pos, Sum_cond, tope, filling = 2772 / 48;
+	char carac_posib[] = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz", pw[tope];
+
+    pw[tope] = '\0';
+
+	srand(time(NULL));
+	while (Sum_cond != 2772)
 	{
-		if (*(s + finder1) == '-')
-			sign *= -1;
-		finder1++;
-	}
-	while ((*(s + finder1) >= '0') && (*(s + finder1) <= '9'))
-	{
-		num = num * 10 + sign * (*(s + finder1) - '0');
-		finder1++;
-	}
-	return (num);
+		Pos_pw = Sum_cond = filling = 0;
+		while ((2650) > Sum_cond)/*2772 - 122 asi no me paso, siendo 122 valor mas alto de ascii de dichos caracteres (z)*/
+		{
+			random_pos = rand() % 62;/*lleno la pw haciendo que la suma del valor ascii de los caracteres no se pase de*/
+			pw[Pos_pw] = carac_posib[random_pos];
+			Sum_cond = Sum_cond + carac_posib[random_pos];
+			Pos_pw++;
+		}
+        pw[Pos_pw] = 2772 - Sum_cond;
+/*		while (carac_posib[filling])        /*Tengo que llenar los ultimos 122 o menos con caracteres para llenar la pw
+*		{
+*			if (carac_posib[filling] == (2772 - Sum_cond))          /*aca checkeo si estoy parado en el caracter que me llena la suma de la pw correcta
+*			{
+*				pw[Pos_pw] = carac_posib[filling];
+*				Sum_cond = Sum_cond + carac_posib[random_pos];
+*				Pos_pw++;
+*				break;            /*para salir del while que filea
+*			}
+*			filling++;           /*me sigo moviendo asi encuentro ese caracter para rellenar la suma hasta 2772
+*		}
+*/	}
+	printf("%s", pw);
+	return (0);
 }
