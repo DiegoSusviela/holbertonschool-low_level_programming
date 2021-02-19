7c51e642d277f6f8957de94e11804dd28e8b7bad
DiegoSusviela <dieguitosus@hotmail.com>
2021-02-19 11:17:14 -0800

    changed to remplace array

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -12,16 +12,13 @@ char *leet(char *str1)
 {
 	int position, count;
 	char be_remplaced[] = "aAeEoOtTlL";
-	char to_remplace[] = "4433007711";
+	char to_remplace[] = "43071";
 
 	while (str1[position] != '\0')
 {
 		for (count = 0; count <= 9; count++)
 			if (be_remplaced[count] == str1[position])
-{
-				str1[position] = to_remplace[count];
-count = 9;
-}
+				str1[position] = to_remplace[count / 2];
 position++;
 }
 return (str1);
