1a6c88892e884685f062a5c2a75befd9ade6ed36
DiegoSusviela <dieguitosus@hotmail.com>
2021-02-19 09:18:50 -0800

    betty likes

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -16,14 +16,14 @@ char *cap_string(char *s)
 	while (s[position] != '\0')
 	{
 		if ((s[position] >= 'a') && (s[position] <= 'z'))
-        {
+{
 			if (position == 0)
 				s[position] = s[position] - 'a' + 'A';
 			else
 				for (count = 0; count <= 12; count++)
 					if (aux[count] == s[position - 1])
 						s[position] = s[position]  - 'a' + 'A';
-        }
+}
 		position++;
 	}
 	return (s);
