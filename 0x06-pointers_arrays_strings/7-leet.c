e9490ec2351e44ccbec4f42359f7cfbd76e5df53
DiegoSusviela <dieguitosus@hotmail.com>
2021-02-19 11:10:23 -0800

    change string name and count in for

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -2,24 +2,27 @@
 
 /**
  * leet - Entry point
- * @s: wopa
+ * @str1: wopa
  *
  * Description: Show a message blablabla
  * Return: Always 0 (Success)
  */
 
-char *leet(char *s)
+char *leet(char *str1)
 {
 	int position, count;
 	char be_remplaced[] = "aAeEoOtTlL";
 	char to_remplace[] = "4433007711";
 
-	while (s[position] != '\0')
+	while (str1[position] != '\0')
 {
 		for (count = 0; count <= 9; count++)
-			if (be_remplaced[count] == s[position])
-				s[position] = to_remplace[count];
+        {
+			if (be_remplaced[count] == str1[position])
+				str1[position] = to_remplace[count];
+            count = 9;
+        }
 position++;
 }
-return (s);
+return (str1);
 }
