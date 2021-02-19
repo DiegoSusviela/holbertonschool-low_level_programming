b133f76276f33eec6de03672d7a41c3df3ba90d1
DiegoSusviela <dieguitosus@hotmail.com>
2021-02-19 13:15:11 -0800

    added first advanced

---
+++ 0x06-pointers_arrays_strings/100-print_number.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ * print_number - imprime
+ * @n: numero a imprimir
+ *
+ * Description: Show a message blablabla
+ * Return: Always 0 (Success)
+ */
+
+void print_number(int n)
+{
+	int resto;
+
+	resto = n % 10;
+	if (n >= 10)
+	{
+		n = n / 10;
+		imprimir(n);
+	}
+	_putchar('0' + resto);
+}
