5f981db1222f15e27f80d2064c6d6049da31927c
DiegoSusviela <dieguitosus@hotmail.com>
2021-02-19 13:22:28 -0800

    es joda?

--- 0x06-pointers_arrays_strings/100-print_number.c
+++ 0x06-pointers_arrays_strings/100-print_number.c
@@ -10,13 +10,20 @@
 
 void print_number(int n)
 {
+	unsigned int num;
 	int resto;
 
-	resto = n % 10;
-	if (n >= 10)
+	if (n < 0)
 	{
-		n = n / 10;
-		print_number(n);
+		_putchar('-');
+		num = -n;
+	}
+	else
+		num = n;
+	if (num >= 10)
+	{
+		num = num / 10;
+		imprimir(num);
 	}
 	_putchar('0' + resto);
 }
