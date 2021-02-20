8252ab02b483c2410cde8a23adb0d229e77c269c
DiegoSusviela <dieguitosus@hotmail.com>
2021-02-20 13:12:31 -0800

    compile

--- 0x06-pointers_arrays_strings/102-infinite_add.c
+++ 0x06-pointers_arrays_strings/102-infinite_add.c
@@ -49,7 +49,7 @@ return (0);
  */
 char *v_v(char *n1, int *p1, int *p2, int *p3, int *rs, char *r, char *n2)
 {
-while ((*p1 > 0) && (*p2 > 0), (*p1)--, (*p2)--)
+while (((*p1 > 0) && (*p2 > 0)), (*p1)--, (*p2)--)
 {
 r[*p3] = '0' + (((n1[*p1] - '0') + (n2[*p2] - '0') + *rs) % 10);
 *rs = calc_rest(n2[*p2] - '0', n1[*p1] - '0', *rs);
@@ -92,7 +92,7 @@ return (r);
 
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-int p1 = _strlen(n1), p2 = _strlen(n2), p3 = (size_r - 2), rs = 0, counter, n;
+int p1 = _strlen(n1), p2 = _strlen(n2), p3 = (size_r - 2), rs = 0, counter;
 
 if ((p1 > (size_r - 1)) || (p2 > (size_r - 1)))
 return (0);
@@ -101,10 +101,12 @@ if (p1 < 0)
 {
 r = viendo_voy(n2, &p2, &p3, &rs, r);
 if ((p2 < 0) && (rs == 1))
+{
 if (p3 < 0)
 return (0);
 else
 r[p3] = '1';
+}
 }
 else
 {
