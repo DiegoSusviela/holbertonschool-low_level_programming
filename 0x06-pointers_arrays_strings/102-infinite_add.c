fd44d7ebc499e7c70cb0e517aad336a39348b292
DiegoSusviela <dieguitosus@hotmail.com>
2021-02-19 17:55:34 -0800

    changed returns 0

--- 0x06-pointers_arrays_strings/102-infinite_add.c
+++ 0x06-pointers_arrays_strings/102-infinite_add.c
@@ -13,47 +13,74 @@
 
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-    int position1, position2, position3, resto, counter;
+    int position1, position2, position3, resto, counter, aux, n;
 
+    position1 = position2 = position3 = 0;
     while (n1[position1] != '\0')
         position1++;
     while (n2[position2] != '\0')
         position2++;
-    if ((position1 > size_r) && (position2 > size_r))
+    if ((position1 > (size_r - 1)) || (position2 > (size_r - 1)))
         return (0);
     position2--;
     position1--;
     position3 = size_r - 2;
     resto = 0;
-    while ((position1 != 0) && (position2 != 0))
+    while ((position1 >= 0) && (position2 >= 0))
     {
-        r[position3] = (n1[position1] + n2[position2] + resto) % 10;
-        if ((n1[position1] + n2[position2]) > 9)
+        n = ((n1[position1] - '0') + (n2[position2] - '0') + resto) % 10;
+        r[position3] = '0' + n;
+        if ( ((n1[position1] - '0') + (n2[position2] - '0') + resto) > 9)
             resto = 1;
-        else 
+        else
             resto = 0;
         position1--;
         position2--;
         position3--;
     }
-    if (position1 = 0)
-        while (position2 < 0)
+    if (position1 < 0)
+    {
+        if ((position2 < 0) && (resto == 1))
+            if (position3 < 0)
+                return (0);
+            else
+                r[position3] = '1';
+        while (position2 >= 0)
         {
-            r[position3] = n2[position2] + resto;
+            aux = ((n2[position2] - '0') + resto) % 10;
+            r[position3] = aux + '0';
+            if (((n2[position2] - '0') + resto) > 9)
+                resto = 1;
+            else
+                resto = 0;
             position2--;
             position3--;
-            resto = 0;
         }
-    else 
-        while (position1 < 0)
+        if ((position2 < 0) && (resto == 1))
+            if (position3 < 0)
+                return (0);
+            else
+                r[position3] = '1';
+    }
+    else
+    {
+        while (position1 >= 0)
         {
-            r[position3] = n1[position1] + resto;
+            aux = ((n1[position1] - '0') + resto) % 10;
+            r[position3] = aux + '0';
+            if (((n1[position1] - '0') + resto) > 9)
+                resto = 1;
+            else
+                resto = 0;
             position1--;
             position3--;
-            resto = 0;
         }
+        if ((resto == 1) && (position3 < 0))
+                return (0);
+        r[position3] = resto + '0';
+    }
     counter = 0;
-    while (position3 != (size_r-1))
+    while (position3 != (size_r - 1))
     {
         r[counter] = r[position3];
         counter++;
