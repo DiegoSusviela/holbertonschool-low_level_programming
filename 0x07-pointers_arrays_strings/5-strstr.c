221813adf7687951ffcd90ad7a431b95ed80f210
DiegoSusviela <dieguitosus@hotmail.com>
2021-02-23 08:53:42 -0800

    betty likes code with no indentation

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -11,22 +11,22 @@
 
 char *_strstr(char *haystack, char *needle)
 {
-    int pos1 = 0, pos2 = 0;
+int pos1 = 0, pos2 = 0;
 
-    while(haystack[pos1])
-    {
-        while(needle[pos2] && haystack[pos1] == needle[0])
-            if (haystack[pos2 + pos1] == needle[pos2])
-                pos2++;
-            else
-                break;
-        if (needle[pos2])
-        {
-            pos1++;
-            pos2 = 0;
-        }
-        else
-            return (haystack + pos1);
-    }
-    return (0);
+while (haystack[pos1])
+{
+while (needle[pos2] && haystack[pos1] == needle[0])
+if (haystack[pos2 + pos1] == needle[pos2])
+pos2++;
+else
+break;
+if (needle[pos2])
+{
+pos1++;
+pos2 = 0;
+}
+else
+return (haystack + pos1);
+}
+return (0);
 }
