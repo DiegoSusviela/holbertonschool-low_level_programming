9d27d42dbe6722b7ae66572825be63c69749f0c3
DiegoSusviela <dieguitosus@hotmail.com>
2021-02-23 08:48:47 -0800

    betty like sno indentention

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -7,23 +7,23 @@
  *
  * Description: Show a message blablabla
  * Return: Always 0 (Success)
- */
+*/
 
 char *_strpbrk(char *s, char *accept)
 {
-    int pos1 = 0, pos2 = 0;
+int pos1 = 0, pos2 = 0;
 
-    while(s[pos1])
-    {
-        while(accept[pos2])
-        {
-            if (s[pos1] == accept[pos2])
-                break;
-        pos2++;
-        }
-        if (accept[pos2])
-            return s[pos1];
-    pos1++;
-    }
-    return (0);
+while (s[pos1])
+{
+while (accept[pos2])
+{
+if (s[pos1] == accept[pos2])
+break;
+pos2++;
+}
+if (accept[pos2])
+return (s + pos1);
+pos1++;
+}
+return (0);
 }
