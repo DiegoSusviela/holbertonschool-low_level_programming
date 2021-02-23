b438f92366ef4357cc348b56eff513f1b42207f4
DiegoSusviela <dieguitosus@hotmail.com>
2021-02-23 11:09:57 -0800

    removed spaces

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -4,7 +4,7 @@
 /**
  *  print_chessboard- imprime
  * @a: tablero
- * 
+ *
  * Description: Show a message blablabla
  * Return: Always 0 (Success)
  */
@@ -15,12 +15,12 @@ int diag1 = 0, diag2 = 0, x, y, aux, aux2;
 
 for (y = 0; y < size; y++)
 {
-aux = (y*size) + y;
+aux = (y * size) + y;
 diag1 += *(a + aux);
 }
 for (x = 0; x < size; x++)
 {
-aux2 = (x*size) + size - 1 - x;
+aux2 = (x * size) + size - 1 - x;
 diag2 += *(a + aux2);
 }
 printf("%d, %d", diag1, diag2);
