3ff885f11ca13d560310e38fdf26dd572a2db4b2
DiegoSusviela <dieguitosus@hotmail.com>
2021-02-23 08:45:29 -0800

    espacios para betty en los while

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -13,10 +13,10 @@ unsigned int _strspn(char *s, char *accept)
 {
 int pos1 = 0, pos2 = 0;
 
-while(s[pos1])
+while (s[pos1])
 {
 pos2 = 0;
-while(accept[pos2])
+while (accept[pos2])
 {
 if (s[pos1] == accept[pos2])
 break;
