56b965d4ca53b99f81c9f35ea1daf0ba3fb0c566
DiegoSusviela <dieguitosus@hotmail.com>
2021-02-23 08:43:51 -0800

    reiniciar el pos2 en el while es un meme

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -11,19 +11,20 @@
 
 unsigned int _strspn(char *s, char *accept)
 {
-    int pos1 = 0, pos2 = 0;
+int pos1 = 0, pos2 = 0;
 
-    while(s[pos1])
-    {
-        while(accept[pos2])
-        {
-            if (s[pos1] == accept[pos2])
-                break;
-        pos2++;
-        }
-        if (!accept[pos2])
-            break;
-    pos1++;
-    }
-    return (pos1);
+while(s[pos1])
+{
+pos2 = 0;
+while(accept[pos2])
+{
+if (s[pos1] == accept[pos2])
+break;
+pos2++;
+}
+if (!accept[pos2])
+break;
+pos1++;
+}
+return (pos1);
 }
