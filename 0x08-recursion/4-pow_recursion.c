ed1b7777c45e89c5bdb391e33083e8c369e2c268
DiegoSusviela <dieguitosus@hotmail.com>
2021-02-24 09:11:58 -0800

    deleted second if

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,7 +1,7 @@
 #include "holberton.h"
 
 /**
- * _pow_recursion - imprime
+ * recursion - imprime
  * @x: numero a imprimir
  * @y: un char ahi
  *
@@ -14,9 +14,17 @@ int recursion(int x, int y)
     if (y > 0)
         return (x  * (recursion(x, (y - 1))));
     else
-        if (y == 0)
-            return (1);
+        return (1);
 }
+
+/**
+ * _pow_recursion - imprime
+ * @x: numero a imprimir
+ * @y: un char ahi
+ *
+* Description: Show a message blablabla
+* Return: Always 0 (Success)
+*/
 
 int _pow_recursion(int x, int y)
 {
