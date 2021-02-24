374a79317493138776a7df5c98dc2e2f9ce3cf6d
DiegoSusviela <dieguitosus@hotmail.com>
2021-02-24 19:29:59 -0300

    more tabs for wendy

--- 0x08-recursion/100-wildcmp.c
+++ 0x08-recursion/100-wildcmp.c
@@ -23,11 +23,9 @@ int wildcmp(char *s1, char *s2)
 	{
 		if (*(s2 + 1) == '*')
 			return (wildcmp(s1, (s2 + 1)));
-		else
-			if (wildcmp(s1, s2 + 1) == 1)
-				return (1);
-			else
-				return (wildcmp(s1 + 1, s2));
+		if (wildcmp(s1, s2 + 1) == 1)
+			return (1);
+		return (wildcmp(s1 + 1, s2));
 	}
 	return (0);
 }
