808e6cd6c040869347abc9dd831a896284535a34
DiegoSusviela <dieguitosus@hotmail.com>
2021-02-24 09:36:46 -0800

    changed logic

--- 0x08-recursion/7-is_palindrome.c
+++ 0x08-recursion/7-is_palindrome.c
@@ -2,18 +2,26 @@
 #include "2-strlen_recursion.c"
 
 /**
- * recursion - imprime
- * @n: numero a imprimir
- * @aux: un char ahi
+ * recursion1 - imprime
+ * @s: numero a imprimir
+ * @largo: un char ahi
+ * @counter: kdfno
  *
 * Description: Show a message blablabla
 * Return: Always 0 (Success)
 */
 
-int recursion1(char s, int largo, int counter)
-    if ((*(s + counter)) == *(s + counter + largo))
-        recursion1((s + 1), largo - 1, counter + 1);
-    
+int recursion1(char *s, int largo, int counter)
+{
+    if (((*(s + counter)) == *(s - counter + largo - 1)) && (counter < (largo / 2)))
+        return (recursion1((s), largo, counter + 1));
+    else
+        if (counter >= (largo / 2))
+            return (1);
+        else
+            return (0);
+}
+
 
 /**
  * is_palindrome - imprime
@@ -23,12 +31,8 @@ int recursion1(char s, int largo, int counter)
 * Return: Always 0 (Success)
 */
 
-
-
 int is_palindrome(char *s)
 {
-
-    largo = _strlen_recursion(s);
-    recursion1(s, largo, 0)
-
+    int largo = _strlen_recursion(s);
+    return (recursion1(s, largo, 0));
 }
