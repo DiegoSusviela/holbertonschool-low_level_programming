0c6e466d341798eea664a2ada80328c69b57c375
DiegoSusviela <dieguitosus@hotmail.com>
2021-02-24 19:25:32 -0300

    Born to be brave

--- 0x08-recursion/100-wildcmp.c
+++ 0x08-recursion/100-wildcmp.c
@@ -1,32 +1,33 @@
 #include "holberton.h"
 
 /**
- * wildcmp - Compare two strings, find if they are matching
- * @s1: String to compare
- * @s2: String to compare, can include * wildcard
+ * wildcmp - compares if im sick of it
+ * @s1: ya sabe
+ * @s2: me aburri
  *
- * Return: 1 if identical, else 0
+ * Return: return of fucntion
  */
 int wildcmp(char *s1, char *s2)
 {
 	if (*s1 == '\0' && *s2 == '\0')
 		return (1);
-	else if (*s1 == '\0' && *s2 == '*')
-		return (wildcmp(s1, (s2 + 1)));
-	else if (*s1 == '\0' || *s2 == '\0')
-		return (0);
-
+	else
+		if (*s1 == '\0' && *s2 == '*')
+			return (wildcmp(s1, (s2 + 1)));
+		else
+			if (*s1 == '\0' || *s2 == '\0')
+				return (0);
 	if (*s1 == *s2)
 		return (wildcmp((s1 + 1), (s2 + 1)));
-
 	if (*s2 == '*')
 	{
 		if (*(s2 + 1) == '*')
 			return (wildcmp(s1, (s2 + 1)));
-		else if (wildcmp(s1, s2 + 1))
-			return (1);
 		else
-			return (wildcmp(s1 + 1, s2));
+			if (wildcmp(s1, s2 + 1) == 1)
+				return (1);
+			else
+				return (wildcmp(s1 + 1, s2));
 	}
 	return (0);
 }
