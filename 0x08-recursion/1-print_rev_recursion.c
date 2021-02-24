21e284d05d91e9f7cc1fc4fb9c0a54c96a3f79e7
DiegoSusviela <dieguitosus@hotmail.com>
2021-02-24 08:38:32 -0800

    switched to only 1 function

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,24 +1,6 @@
 #include "holberton.h"
 
 /**
- * recursion - imprime
- * @s: numero a imprimir
- * @position: un char ahi
- *
-* Description: Show a message blablabla
-* Return: Always 0 (Success)
-*/
-
-void recursion(char *s, int position)
-{
-    if (*(s + position))
-    {
-        recursion(s, position + 1);
-        _putchar(*(s + position));
-    }
-}
-
-/**
  * _print_rev_recursion - imprime
  * @s: numero a imprimir
  *
@@ -28,8 +10,9 @@ void recursion(char *s, int position)
 
 void _print_rev_recursion(char *s)
 {
-    int position = 0;
-
-    recursion(s, position);
-    _putchar('\n');
+    if (*s)
+    {
+        _print_rev_recursion(s + 1);
+        _putchar(*(s));
+    }
 }
