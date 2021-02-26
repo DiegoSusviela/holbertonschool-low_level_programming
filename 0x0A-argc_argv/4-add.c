23c4412fa38e6c9c7e9781450410c378caf3c494
DiegoSusviela <dieguitosus@hotmail.com>
2021-02-26 09:42:50 -0800

    added 4

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * @argc: wopa
+ * @argv: sdkna
+ *
+ * Description: Show a message blablabla
+ * Return: Always 0 (Success)
+ */
+
+int main(int argc, char *argv[])
+{
+	int i, aux, suma = 0;
+
+	for(i = 1; i < argc; i++)
+		if (argc[i] >= '0' && argc[i] <= '9')
+		{
+			aux = atoi(argc[i]);
+			suma = argc[i] + suma;
+		}
+		else
+		{
+			printf("Error\n");
+			return(1);
+		}
+		printf("%d\n", suma);
+		return (0);
+}
