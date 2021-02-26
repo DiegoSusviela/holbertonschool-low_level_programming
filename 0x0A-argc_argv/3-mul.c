9b929a366e6a42764390d4c294a38803a94bf18a
DiegoSusviela <dieguitosus@hotmail.com>
2021-02-26 09:13:21 -0800

    added 3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ * @argc: wopa
+ * @argv: sdkna
+ *
+ * Description: Show a message blablabla
+ * Return: Always 0 (Success)
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argv[1] * argv[2]);
+	return (0);
+}
