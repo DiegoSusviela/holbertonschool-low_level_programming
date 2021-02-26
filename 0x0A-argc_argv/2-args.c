68e953e099eea51da9de3e245a0c626bc1a77efd
DiegoSusviela <dieguitosus@hotmail.com>
2021-02-26 08:57:08 -0800

    added 2

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ * @argc: wopa
+ * @argv: sdkna
+ *
+ * Description: Show a message blablabla
+ * Return: Always 0 (Success)
+ */
+
+int main(int argc, char *argv[])
+{
+    int i;
+
+    for(i = 0; i < argc - 1; i++)
+    {
+        printf("%s", *argv[i]);
+    }
+    return 0;
+}
