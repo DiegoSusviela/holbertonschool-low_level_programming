a348b8a64293ae38a082ddef784621fd422a792f
DiegoSusviela <dieguitosus@hotmail.com>
2021-02-27 14:02:12 -0300

    changed aux assigantion

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -12,38 +12,39 @@
 
 int main(int argc, char *argv[])
 {
-	int aux = atoi(argv[1]), suma = 0, cantidad_monedas = 0;
+	int aux = 0, suma = 0, cantidad_monedas = 0;
 
 	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
-		while (suma + 25 <= aux)
-		{
-			suma = suma + 25;
-			cantidad_monedas = cantidad_monedas + 1;
-		}
-			while (suma + 10 <= aux)
-			{
-				suma = suma + 10;
-				cantidad_monedas = cantidad_monedas + 1;
-			}
-				while (suma + 5 <= aux)
-				{
-					suma = suma + 5;
-					cantidad_monedas = cantidad_monedas + 1;
-				}
-					while (suma + 2 <= aux)
-					{
-						suma = suma + 2;
-						cantidad_monedas = cantidad_monedas + 1;
-					}
-					while (suma + 1 <= aux)
-					{
-						suma = suma + 1;
-						cantidad_monedas = cantidad_monedas + 1;
-					}
+	aux = atoi(argv[1])
+	while (suma + 25 <= aux)
+	{
+		suma = suma + 25;
+		cantidad_monedas = cantidad_monedas + 1;
+	}
+	while (suma + 10 <= aux)
+	{
+		suma = suma + 10;
+		cantidad_monedas = cantidad_monedas + 1;
+	}
+	while (suma + 5 <= aux)
+	{
+		suma = suma + 5;
+		cantidad_monedas = cantidad_monedas + 1;
+	}
+	while (suma + 2 <= aux)
+	{
+		suma = suma + 2;
+		cantidad_monedas = cantidad_monedas + 1;
+	}
+	while (suma + 1 <= aux)
+	{
+		suma = suma + 1;
+		cantidad_monedas = cantidad_monedas + 1;
+	}
 	printf("%d\n", cantidad_monedas);
 	return (0);
 }
