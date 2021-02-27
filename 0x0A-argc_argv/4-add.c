a742fb8a4ee4903a100a162738f63af9c9e3ada8
DiegoSusviela <dieguitosus@hotmail.com>
2021-02-27 13:50:10 -0300

    added conditiopn for negative numbers

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -15,10 +15,16 @@ int main(int argc, char *argv[])
 	int i, aux, suma = 0;
 
 	for (i = 1; i < argc; i++)
-		if (atoi(argv[i]) >= 0 && atoi(argv[i]) <= 9)
+		if (*argv[i] >= '0' && *argv[i] <= '9')
 		{
 			aux = atoi(argv[i]);
-			suma = aux + suma;
+			if (aux >= 0)
+				suma = aux + suma;
+			else 
+				{
+					printf("Error\n");
+				return (1);
+				}
 		}
 		else
 		{
