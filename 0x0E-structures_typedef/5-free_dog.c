b186220401f32daaa0094afc5eb8c95aa2cb02a2
DiegoSusviela <dieguitosus@hotmail.com>
2021-03-08 09:32:33 -0800

    added 5

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * free_dog - Entry point
+ * @d: ea
+ *
+ * Description: Show a message blablabla
+ * Return: Always 0 (Success)
+ */
+void free_dog(dog_t *d)
+{
+	if (d)
+		free(d->name);
+		free(d->owner);
+		free(d);
+}
