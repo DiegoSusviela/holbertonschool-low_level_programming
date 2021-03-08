abde6810d82d63038843a0a6512fcbdcc282c71c
DiegoSusviela <dieguitosus@hotmail.com>
2021-03-08 09:47:42 -0800

    changed nill to nil

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -16,16 +16,11 @@ void print_dog(struct dog *d)
 		if (d->name)
 			printf("Name: %s\n", d->name);
 		else
-			printf("Name: (nill)\n");
-		/*if (d->age)*/
-			printf("Age: %.6f\n", d->age);
-		/*
-		*else
-		*	printf("Age: (nill)");
-		**/
+			printf("Name: (nil)\n");
+		printf("Age: %.6f\n", d->age);
 		if (d->owner)
 			printf("Owner: %s\n", d->owner);
 		else
-			printf("Owner: (nill)\n");
+			printf("Owner: (nil)\n");
 	}
 }
