bc665e398a6f33deca51c67b13d7a7301b068e71
DiegoSusviela <dieguitosus@hotmail.com>
2021-03-08 08:58:45 -0800

    added an if condition for the age

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -20,7 +20,10 @@ void print_dog(struct dog *d)
 			printf("Name: %s\n", d->name);
 		else
 			printf("Name: (nill)\n");
-		printf("Age: %.6f\n", d->age);
+		if (d->age)
+			printf("Age: %.6f\n", d->age);
+		else
+			printf("Age: (nill)");
 		if (d->owner)
 			printf("Owner: %s\n", d->owner);
 		else
