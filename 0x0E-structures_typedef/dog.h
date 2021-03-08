babce94118209daa2b126a69a6bb03070ab8a9e3
DiegoSusviela <dieguitosus@hotmail.com>
2021-03-08 08:33:58 -0800

    added 3 and changed some betty things

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,15 @@
 #ifndef _DOG_
 #define _DOG_
+
+/**
+ * struct dog - Short description
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ *
+ * Description: Longer description
+ */
+
 struct dog
 {
 	char *name;
@@ -7,6 +17,14 @@ struct dog
 	char *owner;
 };
 
-typedef struct dog my_dog;
+/**
+ * dog_t - Typedef for unsigned int
+ */
+
+typedef struct dog dog_t;
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 
 #endif
