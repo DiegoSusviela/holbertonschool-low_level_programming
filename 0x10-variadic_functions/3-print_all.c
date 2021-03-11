ddb280d2a00539595dc64264450c3f92072e2bf8
DiegoSusviela <dieguitosus@hotmail.com>
2021-03-11 11:07:46 -0800

    betty likes

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,27 +1,51 @@
 #include "variadic_functions.h"
 
 /**
- * print_strings - imprime
- * @separator: numero a imprimir
- * @n: some number idk tbf
+ * print_char - imprime
+ * @list: numero a imprimir
  *
 * Description: Show a message blablabla
 * Return: Always 0 (Success)
 */
+
 void print_char(va_list list)
 {
 	printf("%c", va_arg(list, int));
 }
+
+/**
+ * print_int - imprime
+ * @list: numero a imprimir
+ *
+* Description: Show a message blablabla
+* Return: Always 0 (Success)
+*/
 
 void print_int(va_list list)
 {
 	printf("%d", va_arg(list, int));
 }
 
+/**
+ * print_float - imprime
+ * @list: numero a imprimir
+ *
+* Description: Show a message blablabla
+* Return: Always 0 (Success)
+*/
+
 void print_float(va_list list)
 {
-	printf("%f", va_arg(list, double));
+	printf("%f", va_arg(list, float));
 }
+
+/**
+ * print_char_2 - imprime
+ * @list: numero a imprimir
+ *
+* Description: Show a message blablabla
+* Return: Always 0 (Success)
+*/
 
 void print_char_2(va_list list)
 {
@@ -37,6 +61,13 @@ void print_char_2(va_list list)
 	}
 }
 
+/**
+ * print_all - imprime
+ * @format: numero a imprimir
+ *
+* Description: Show a message blablabla
+* Return: Always 0 (Success)
+*/
 
 void print_all(const char * const format, ...)
 {
@@ -48,18 +79,17 @@ void print_all(const char * const format, ...)
 		{NULL, NULL},
 	};
 
-	int index_tuvieja, i = 0;
+	int index_tuvieja, iterador = 0;
 	va_list list;
 	char *separator = "";
-
 	va_start (list, format);
 
-	while (format && format[i])
+	while (format && format[iterador])
 	{
 		index_tuvieja = 0;
 		while (tu_vieja[index_tuvieja].type)
 		{
-			if (*tu_vieja[index_tuvieja].type == format[i])
+			if (*tu_vieja[index_tuvieja].type == format[iterador])
 			{
 				printf("%s", separator);
 				tu_vieja[index_tuvieja].func(list);
@@ -67,7 +97,7 @@ void print_all(const char * const format, ...)
 			}
 			index_tuvieja++;
 		}
-		i++;
+		iterador++;
 	}
 	printf("\n");
 	va_end(list);
