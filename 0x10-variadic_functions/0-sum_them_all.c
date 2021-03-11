c781ffb921b9b76ff31193f915ea559c8d9077c7
DiegoSusviela <dieguitosus@hotmail.com>
2021-03-11 08:25:09 -0800

    addedsum of variadic functions

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,26 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+
+/**
+ * recursion - imprime
+ * @s: numero a imprimir
+ * @position: un char ahi
+ *
+* Description: Show a message blablabla
+* Return: Always 0 (Success)
+*/
+
+int sum_them_all(const unsigned int n, ...)´
+{
+	va_list sum_list;
+	unsigned int asum;
+	int sum = 0;
+
+	if (n == 0)
+		return (0);
+	va_start(sum_list, n);
+	for (asum = 0; asum < n; asum++)
+		sum += va_arg( sum_list, int );
+	va_end(sum_list);
+	return (sum);
+}
---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,26 @@
+#ifndef VARIADIC_FUNCTIONS_H
+#define VARIADIC_FUNCTIONS_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * struct datatype - imprime
+ * @type: numero a imprimir
+ * @func: un char ahi
+ *
+* Description: Show a message blablabla
+* Return: Always 0 (Success)
+*/
+
+typedef struct datatype
+{
+	char *type;
+	void (*func)(va_list list);
+}data_t;
+
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+int _putchar(char c);
