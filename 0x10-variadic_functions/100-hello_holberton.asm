73f58860038eedb031027bb56f9ac5ef38ecacd4
DiegoSusviela <dieguitosus@hotmail.com>
2021-03-11 19:22:24 -0300

    body of assebly code, kill me before i start

--- 0x10-variadic_functions/100-hello_holberton.asm
+++ 0x10-variadic_functions/100-hello_holberton.asm
@@ -1,27 +1,9 @@
-        .file   "helo_hol.c"
-        .section        .rodata
-.LC0:
-        .string "Hello, Holberton\n"
-        .text
-        .globl  main
-        .type   main, @function
+section .text
+	global main
 main:
-.LFB2:
-        .cfi_startproc
-        pushq   %rbp
-        .cfi_def_cfa_offset 16
-        .cfi_offset 6, -16
-        movq    %rsp, %rbp
-        .cfi_def_cfa_register 6
-        movl    $17, %edx
-        movl    $.LC0, %esi
-        movl    $1, %edi
-        call    write
-        popq    %rbp
-        .cfi_def_cfa 7, 8
-        ret
-        .cfi_endproc
-.LFE2:
-        .size   main, .-main
-        .ident  "GCC: (Ubuntu 4.8.4-2ubuntu1~14.04.4) 4.8.4"
-        .section        .note.GNU-stack,"",@progbits
+
+
+
+
+section .data
+	msg db 'Hello, Holberton',0xa
