9bb9df0fac511c73d305c1545af48e5e6f03ebdf
DiegoSusviela <dieguitosus@hotmail.com>
2021-03-18 13:59:46 -0300

    fixing betty, adding comments to functions

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,10 +1,18 @@
 #include "lists.h"
 #include "basic_operations.c"
 
+/**
+ * print_list - imprime
+ * @h: some number idk tbf
+ *
+* Description: Show a message blablabla
+* Return: Always 0 (Success)
+*/
+
 size_t print_list(const list_t *h)
 {
 	int count = 0;
-	
+
 	while (es_localizador_cadena(h))
 	{
 		print_node(h);
--- 0x12-singly_linked_lists/basic_operations.c
+++ 0x12-singly_linked_lists/basic_operations.c
@@ -1,6 +1,12 @@
 #include "lists.h"
 
-/*typedef list_t *localizador;*/
+/**
+ * siguiente - imprime
+ * @loc: some number idk tbf
+ *
+* Description: Show a message blablabla
+* Return: Always 0 (Success)
+*/
 
 const list_t *siguiente(const list_t *loc)
 {
@@ -10,6 +16,14 @@ const list_t *siguiente(const list_t *loc)
 		return (loc->next);
 }
 
+/**
+ * es_final_cadena - imprime
+ * @loc: some number idk tbf
+ *
+* Description: Show a message blablabla
+* Return: Always 0 (Success)
+*/
+
 const list_t *es_final_cadena(const list_t *loc)
 {
 	if (es_localizador_cadena(loc->next))
@@ -17,12 +31,28 @@ const list_t *es_final_cadena(const list_t *loc)
 	return (loc);
 }
 
+/**
+ * es_localizador_cadena - imprime
+ * @loc: some number idk tbf
+ *
+* Description: Show a message blablabla
+* Return: Always 0 (Success)
+*/
+
 const list_t *es_localizador_cadena(const list_t *loc)
 {
 	if (loc)
 		return (loc);
 	return (NULL);
 }
+
+/**
+ * print_node - imprime
+ * @loc: some number idk tbf
+ *
+* Description: Show a message blablabla
+* Return: Always 0 (Success)
+*/
 
 void print_node(const list_t *loc)
 {
