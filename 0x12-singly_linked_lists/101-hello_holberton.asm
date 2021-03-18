a403971deb46a4faf5d33c225e76de6dd2ad0011
DiegoSusviela <dieguitosus@hotmail.com>
2021-03-18 16:44:51 -0300

    added some more comments

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -2,11 +2,11 @@ section .text							;kill me plox, im doing this the hard way AGAIN
 	extern printf
 	global main							;linker declaration
 main:									;linker entry
-	mov	rdi, msg_to_print
+	mov	rdi, msg_to_print				;source index
 	mov rax, 0							;sys call yay... in betweeen the printf call
 	call printf							;our call to printf
 	mov rax, 0							;sys call again, idk a guy in stackoverflow did it, i dont really give a s*t ONCE AGAIN
-	ret
+	ret									;return from the procedure
 
 msg_to_print:
 	db "Hello, Holberton", 10, 0		;our bloddy printing, im sick of doing things in asssembly when theres an easy way out there
