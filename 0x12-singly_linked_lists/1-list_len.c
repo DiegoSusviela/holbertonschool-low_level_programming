5c9989f0da73e2284e253f6585fa017d6ff86460
DiegoSusviela <dieguitosus@hotmail.com>
2021-03-18 14:03:06 -0300

    added 1

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * list_len - imprime
+ * @h: some number idk tbf
+ *
+* Description: Show a message blablabla
+* Return: Always 0 (Success)
+*/
+
+size_t list_len(const list_t *h)
+{
+	int count = 0;
+
+	while (es_localizador_cadena(h))
+	{
+		count++;
+		h = siguiente(h);
+	}
+	return (count);
+}
