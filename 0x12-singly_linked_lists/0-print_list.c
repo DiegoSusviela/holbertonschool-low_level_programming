88c665f8c3670a534dc1871975b56eba033703c0
DiegoSusviela <dieguitosus@hotmail.com>
2021-03-18 12:47:57 -0300

    added first, basic operators and some defines

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+size_t print_list(const list_t *h)
+{
+	int count = 1;
+	if (es_localizador_cadena(h) == true)
+		print_node(h);
+	else 
+	{
+		printf("[0] (nil)");
+		return (0);
+	}
+	h = siguiente(h);
+	while (es_localizador_cadena(h) == true)
+	{
+		print_node(h);
+		h = siguiente(h);
+		count++;
+	}
+	return (count);
+}
---
+++ 0x12-singly_linked_lists/basic_operations.c
@@ -0,0 +1,24 @@
+localizador siguiente(localizador loc) {
+	if (es_final_cadena(loc) == true)
+		return (NULL);
+	else
+		return (loc->next);
+}
+
+bool es_final_cadena(localizador loc)
+{
+	if (loc->next)
+		return (false);
+	return (true);
+}
+
+boolean es_localizador_cadena(localizador loc) {
+	if (loc)
+		return (true);
+	return (false);
+}
+
+void print_node(localizador loc)
+{
+	printf("[%d] %s", loc->len, loc->str);
+}
