b70efda617e70f9117adb859cd7ac777e758e87f
DiegoSusviela <dieguitosus@hotmail.com>
2021-03-18 16:23:52 -0300

    added 1 advanced

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * first - prints something before main
+ *
+ * Return: void
+ */
+void first(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
