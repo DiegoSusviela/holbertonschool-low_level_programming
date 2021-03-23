77fb2c4e055af73c8b7068a1bc58ec4266f8e591
DiegoSusviela <dieguitosus@hotmail.com>
2021-03-23 07:33:45 -0700

    lets see if checker likes includes

---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+
+/**
+ * delete_nodeint_at_index - imprime
+ * @head: some number idk tbf
+ * @index: alndl
+ *
+* Description: Show a message blablabla
+* Return: Always 0 (Success)
+*/
+
+listint_t *reverse_listint(listint_t **head)
+{
+	listint_t *loc1, *loc2;
+
+	if (!head)
+		return (NULL);
+	loc1 = *head;
+	loc2 = siguiente(*head);
+	*head = NULL;
+
+	while (es_localizador_cadena(loc2))
+	{
+		instertar_en_cadena(head, loc1);
+		loc1 = loc2;
+		loc2 = siguiente(loc2);
+	}
+	instertar_en_cadena(head, loc1);
+	return (*head);
+}
