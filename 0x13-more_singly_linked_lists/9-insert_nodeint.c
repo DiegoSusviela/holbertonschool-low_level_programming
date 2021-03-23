df3792bb6ccc04f30cc2415a0369888b12a35980
DiegoSusviela <dieguitosus@hotmail.com>
2021-03-23 06:37:46 -0700

    testing change of condition

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -15,22 +15,12 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *aux = *head;
 	listint_t *a_insertar;
 
-	if (idx == 0)
-	{
-		a_insertar = nuevo_nodo(n);
-
-		if (!a_insertar)
-			return (NULL);
-		instertar_en_cadena(head, a_insertar);
-		return (a_insertar);
-	}
-
 	while (es_localizador_cadena(aux) && idx > 1)
 	{
 		aux = siguiente(aux);
 		idx--;
 	}
-	if (!es_localizador_cadena(aux))
+	if (!es_localizador_cadena(aux) && idx != 0)
 		return (NULL);
 
 	a_insertar = nuevo_nodo(n);
