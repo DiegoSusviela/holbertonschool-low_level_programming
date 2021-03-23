82b35cfa5c39dfd6784f12f8979e0d68be3f5544
DiegoSusviela <dieguitosus@hotmail.com>
2021-03-23 09:45:49 -0700

    added 102 advanced

---
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,47 @@
+#include "lists.h"
+
+/**
+ * free_listint_safe - Entry point
+ * @h: wopa
+ *
+ * Description: Show a message blablabla
+ * Return: Always 0 (Success)
+ */
+
+size_t free_listint_safe(listint_t **h)
+{
+	listint_t *loc1, *loc2 = NULL;
+	int count = 0;
+
+	if (!h)
+		return (count);
+	loc1 = *h;
+	if (!*h)
+		return (count);
+	loc2 = siguiente(*h);
+	*h = NULL;
+
+	while (es_localizador_cadena(loc2) && siguiente(loc2) < loc2)
+	{
+		liberar_nodo(loc1);
+		loc1 = loc2;
+		loc2 = siguiente(loc2);
+		count++;
+	}
+	liberar_nodo(loc1);
+	count++;
+	loc1 = loc2;
+	loc2 = siguiente(loc2);
+	if (es_localizador_cadena(loc1))
+	{
+		print_mem_loc(loc1);
+		print_node(loc1);
+		printf("-> ");
+		count++;
+		loc1 = siguiente(loc1);
+		print_mem_loc(loc1);
+		print_node(loc1);
+	}
+	h = NULL;
+	return (count);
+}
