62424983076f36f494bf9a5d84ba4cd61529354c
DiegoSusviela <dieguitosus@hotmail.com>
2021-03-23 07:58:00 -0700

    tehres an infinite loop i need to fix prob

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,31 @@
+#include "lists.h"
+#include "basic_operations.c"
+
+
+void print_mem_loc(listint_t *loc1)
+{
+	printf("[%p] ", &loc1);
+}
+
+/**
+ * print_listint_safe - imprime
+ * @head: some number idk tbf
+ *
+* Description: Show a message blablabla
+* Return: Always 0 (Success)
+*/
+
+size_t print_listint_safe(const listint_t *head)
+{
+	int count = 0;
+	listint_t *loc1 = head;
+
+	while (es_localizador_cadena(loc1))
+	{
+		print_mem_loc(loc1);
+		print_node(loc1);
+		loc1 = siguiente(loc1);
+		count++;
+	}
+	return (count);
+}
