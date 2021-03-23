049e5c34925a0e5a0201716c55c745aa18c7457a
DiegoSusviela <dieguitosus@hotmail.com>
2021-03-23 10:18:02 -0700

    changed logic a bit

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -19,23 +19,20 @@ size_t free_listint_safe(listint_t **h)
 		return (count);
 	loc2 = siguiente(*h);
 
-	while (es_localizador_cadena(loc2) && siguiente(loc2) < loc2)
+	while (es_localizador_cadena(loc1) && siguiente(loc1) < loc1)
 	{
+		loc2 = loc1;
 		liberar_nodo(loc1);
 		loc1 = loc2;
-		loc2 = siguiente(loc2);
 		count++;
 	}
-	liberar_nodo(loc1);
-	count++;
-	loc1 = loc2;
 	if (es_localizador_cadena(loc1))
 	{
 		liberar_nodo(loc1);
-		loc1 = NULL;
-		loc2 = NULL;
 		count++;
 	}
+	loc1 = NULL;
+	loc2 = NULL;
 	*h = NULL;
 	return (count);
 }
