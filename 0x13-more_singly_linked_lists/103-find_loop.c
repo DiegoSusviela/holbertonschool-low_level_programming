c0f9a318e2b04a821a01bd4e839e869b7e09d94e
DiegoSusviela <dieguitosus@hotmail.com>
2021-03-23 10:59:34 -0700

    change asignation of loc 1 to under if check

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -11,10 +11,11 @@
 
 listint_t *find_listint_loop(listint_t *head)
 {
-	listint_t *loc1 = head;
+	listint_t *loc1;
 
 	if (!head)
 		return (NULL);
+	loc1 = head;
 	while (es_localizador_cadena(loc1) && siguiente(loc1) < loc1)
 		loc1 = siguiente(loc1);
 	if (es_localizador_cadena(loc1))
