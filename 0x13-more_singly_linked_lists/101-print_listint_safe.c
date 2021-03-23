8294a379b97324b77cf3b7d3ca12123f2939b56d
DiegoSusviela <dieguitosus@hotmail.com>
2021-03-23 08:23:15 -0700

    trying memory things (?

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -20,12 +20,22 @@ size_t print_listint_safe(const listint_t *head)
 	int count = 0;
 	const listint_t *loc1 = head;
 
-	while (es_localizador_cadena(loc1))
+	while (es_localizador_cadena(loc1) && siguiente(loc1) < loc1)
 	{
 		print_mem_loc(loc1);
 		print_node(loc1);
 		loc1 = siguiente(loc1);
 		count++;
+	}
+	if (es_localizador_cadena(loc1))
+	{
+		print_mem_loc(loc1);
+		print_node(loc1);
+		printf("-> ");
+		count++;
+		loc1 = siguiente(loc1);
+		print_mem_loc(loc1);
+		print_node(loc1);
 	}
 	return (count);
 }
