ffc9c9d84f9b82c48819c88680d857b844fba944
DiegoSusviela <dieguitosus@hotmail.com>
2021-03-23 12:21:34 -0700

    changing logic to stop messing thi mem pos

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -2,42 +2,70 @@
 #include "5-free_listint2.c"
 
 /**
- * free_listint_safe - Entry point
- * @h: wopa
+ * c_l - imprime
+ * @old: some number idk tbf
+ * @size: some number idk tbf
+ * @loc: some number idk tbf
  *
- * Description: Show a message blablabla
- * Return: Always 0 (Success)
- */
+* Description: Show a message blablabla
+* Return: Always 0 (Success)
+*/
 
-size_t free_listint_safe(listint_t **h)
+const listint_t **c_l(const listint_t **old, size_t size, const listint_t *loc)
 {
-	listint_t *loc1, *loc2 = NULL;
-	int count = 0;
+	const listint_t **new_list;
+	size_t iter;
 
-	if (!h)
-		return (count);
-	loc1 = *h;
-	if (!*h)
-		return (count);
-	loc2 = siguiente(*h);
+	new_list = malloc(size * sizeof(listint_t *));
+	if (!new_list)
+	{
+		free(old);
+		exit(98);
+	}
+	for (iter = 0; iter < size - 1; iter++)
+		new_list[iter] = old[iter];
+	new_list[iter] = loc;
+	free(old);
+	return (new_list);
+}
 
-	while (es_localizador_cadena(loc2) && siguiente(loc2) < loc2)
+/**
+ * free_listint_safe - imprime
+ * @head: some number idk tbf
+ *
+* Description: Show a message blablabla
+* Return: Always 0 (Success)
+*/
+
+size_t free_listint_safe(listint_t **head)
+{
+	size_t iter, count = 0;
+	const listint_t **list = NULL;
+	listint_t *loc;
+
+
+	if (!head || !*head )
+		return (count);
+
+	while (es_localizador_cadena(head))
 	{
-		liberar_nodo(loc1);
-		loc1 = loc2;
-		loc2 = siguiente(loc2);
+		iter = 0;
+		while (iter < count)
+		{
+			if (head == list[iter])
+			{
+				*head = NULL;
+				free(list);
+				return (count);
+			}
+			iter++;
+		}
 		count++;
+		list = c_l(list, count, head);
+		loc = siguiente (*head);
+		free(*head);
+		*head = loc;
 	}
-	liberar_nodo(loc1);
-	count++;
-	loc1 = loc2;
-	if (es_localizador_cadena(loc1))
-	{
-		liberar_nodo(loc1);
-		loc1 = NULL;
-		loc2 = NULL;
-		count++;
-	}
-	*h = NULL;
+	free(list);
 	return (count);
 }
