f3a2c9d3c9fdd21810577256f138f8c066705de1
DiegoSusviela <dieguitosus@hotmail.com>
2021-03-25 15:15:31 -0300

    added1

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+
+/**
+ * print_binary - omaiga
+ * @n: wopa
+ *
+ * Return: the returnas
+ */
+
+void print_binary(unsigned long int n)
+{
+	int acom, aux, tope = 0;
+
+	if (n == 0)
+	{
+		_putchar('0');
+		return;
+	}
+	for (acom = (2^6) - 1; acom > -1; acom--)
+	{
+		aux = n >> acom;
+		if (aux & 1)
+		{
+			tope = 1;
+			_putchar('1');
+		}
+		else
+			if (tope == 1)
+				_putchar('0');
+	}
+}
