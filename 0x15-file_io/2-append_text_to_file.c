e51637e98bb3c49e420d832534cb5fd8fefdc7ae
DiegoSusviela <dieguitosus@hotmail.com>
2021-03-29 14:57:57 -0300

    added condition of no text content before open

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -31,18 +31,19 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (!filename)
 		return (-1);
 
+	if (!text_content)
+		return (1);
+
 	file_desc = open(filename, O_WRONLY | O_APPEND);
 
 	if (file_desc == -1)
 		return (-1);
 
-	if (!text_content)
-		largo = write(file_desc, text_content, _strlen(text_content));
-
-	close(file_desc);
+	largo = write(file_desc, text_content, _strlen(text_content));
 
 	if (largo == -1)
 		return (-1);
 
+	close(file_desc);
 	return (1);
 }
