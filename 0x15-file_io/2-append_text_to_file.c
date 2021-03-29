4fe5d463272e78621b0cbf7486b9aedbf283138e
DiegoSusviela <dieguitosus@hotmail.com>
2021-03-29 14:55:04 -0300

    added 2

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,48 @@
+#include "holberton.h"
+
+/**
+ * _strlen - omaiga
+ * @str: wopa
+ *
+ * Return: the returnas
+ */
+
+size_t _strlen(char *str)
+{
+	size_t pos = 0;
+
+	while (str[pos])
+		pos++;
+	return (pos);
+}
+/**
+ * append_text_to_file - omaiga
+ * @filename: wopa
+ * @text_content: dsfdsf
+ *
+ * Return: the returnas
+ */
+
+int append_text_to_file(const char *filename, char *text_content)
+{
+	ssize_t largo = 0;
+	int file_desc;
+
+	if (!filename)
+		return (-1);
+
+	file_desc = open(filename, O_WRONLY | O_APPEND);
+
+	if (file_desc == -1)
+		return (-1);
+
+	if (!text_content)
+		largo = write(file_desc, text_content, _strlen(text_content));
+
+	close(file_desc);
+
+	if (largo == -1)
+		return (-1);
+
+	return (1);
+}
