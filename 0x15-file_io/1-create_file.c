c217a937a1add4ea6bd94c4c60b1e8ea41f5c0e0
DiegoSusviela <dieguitosus@hotmail.com>
2021-03-29 13:49:48 -0300

    removed extra tab on return line 41

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -38,7 +38,7 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 
 	if (!text_content)
-			return (1);
+		return (1);
 
 	largo = write(file_desc, text_content, _strlen(text_content));
 
