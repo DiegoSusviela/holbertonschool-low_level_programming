921b871deefd32efe778e434fbf35a3fbd53ee9c
DiegoSusviela <dieguitosus@hotmail.com>
2021-03-29 13:45:08 -0300

    calling strl outside write

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -36,9 +36,9 @@ int create_file(const char *filename, char *text_content)
 
 	if (file_desc == -1)
 		return (-1);
-
+	largo = _strlen(text_content);
 	if (!text_content)
-		largo = write(file_desc, text_content, _strlen(text_content));
+		largo = write(file_desc, text_content, largo);
 	close(file_desc);
 
 	if (largo == -1)
