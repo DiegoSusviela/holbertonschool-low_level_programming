853bf099b22e4aac1b96dfe2bd7c12a9e70d3ed1
DiegoSusviela <dieguitosus@hotmail.com>
2021-04-30 15:45:06 -0300

    idk why thius aint working

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -11,9 +11,10 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *nuevo_nodo = NULL;
 
-	if (!head || !crear_nodo(nuevo_nodo, n))
+	if (!head)
 		return (NULL);
-	
+	if (!crear_nodo(nuevo_nodo, n))
+		return (NULL);
 	if (es_loc_cad(*head))
 		insertar_antes(nuevo_nodo, *head);
 	*head = nuevo_nodo;
