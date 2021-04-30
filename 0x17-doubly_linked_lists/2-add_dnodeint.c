12710f34850feb5c907e90eb60b145df912f6ee6
DiegoSusviela <dieguitosus@hotmail.com>
2021-04-30 15:06:45 -0300

    added 2

---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+#include "handler_1.c"
+
+/**
+ * add_dnodeint - sdadsa
+ * @head: asdasd
+ * @n: asdasd
+ * Return: asdas
+ */
+
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *nuevo_nodo = NULL;
+
+	if (!head || !crear_nodo(nuevo_nodo, n))
+		return (NULL);
+	
+	insertar_antes(nuevo_nodo, *head);
+	*head = nuevo_nodo;
+	return (nuevo_nodo);
+}
--- 0x17-doubly_linked_lists/handler_1.c
+++ 0x17-doubly_linked_lists/handler_1.c
@@ -13,7 +13,8 @@ dlistint_t *anterior(const dlistint_t *loc)
 void insertar_antes(dlistint_t *a_insertar, dlistint_t *lugar)
 {
 	a_insertar->next = lugar;
-	lugar->prev->next = a_insertar;
+	if (es_loc_cad(anterior(lugar)))
+		lugar->prev->next = a_insertar;
 	a_insertar->prev = anterior(lugar);
 	lugar->prev = a_insertar;
 }
@@ -37,3 +38,25 @@ const dlistint_t *es_loc_cad(const dlistint_t *loc)
 		return (loc);
 	return (NULL);
 }
+
+dlistint_t *crear_nodo(dlistint_t *nodo, const int n)
+{
+	nodo = malloc(sizeof(dlistint_t));
+	if (!nodo)
+		return (NULL);
+	nodo->n = n;
+	nodo->prev = NULL;
+	nodo->next = NULL;
+
+	return (nodo);
+}
+/*
+void insertar_inicio(dlistint_t *a_insertar, dlistint_t *head)
+{
+	if (head)
+	{
+		
+	}
+		head = a_insertar;
+	
+}*/
