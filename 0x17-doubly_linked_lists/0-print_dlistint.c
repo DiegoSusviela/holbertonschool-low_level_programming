88172d3834db3720d235a3f5168c9599a997860c
DiegoSusviela <dieguitosus@hotmail.com>
2021-04-30 14:06:40 -0300

    forgot return 0

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -10,6 +10,9 @@
 size_t print_dlistint(const dlistint_t *h)
 {
 	if (es_loc_cad(h))
+	{
 		imprimir_nodo(h);
-	return (print_dlistint(siguiente(h) + 1));
+		return (print_dlistint(siguiente(h) + 1));
+	}
+	return (0);
 }
