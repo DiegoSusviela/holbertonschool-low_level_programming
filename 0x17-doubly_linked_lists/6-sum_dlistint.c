afc13f81e926d6f96d0f34f3a1640a0dcd8b8d1a
DiegoSusviela <dieguitosus@hotmail.com>
2021-05-01 17:27:39 -0300

    added 6

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+
+/**
+ * free_dlistint - imprime
+ * @head: some number idk tbf
+ *
+* Description: Show a message blablabla
+* Return: Always 0 (Success)
+*/
+
+int sum_dlistint(dlistint_t *head)
+{
+	if (head)
+		return (sum_dlistint(siguiente(head)) + head->n);
+	return (0);	
+}
