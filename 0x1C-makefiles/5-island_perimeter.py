ed5b329bf88df0b1d1496acbb213a7238f39e89a
DiegoSusviela <dieguitosus@hotmail.com>
2021-07-02 20:23:23 -0300

    added another line empty between class coment def

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """ Class that contains the entry point of the command interpreter"""
+
+
 def island_perimeter(grid):
     """ Class that contains the entry point of the command interpreter"""
     perimetro = 0
