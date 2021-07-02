33f1a01fd563f22f86d4aaacc4e95e595318cae9
DiegoSusviela <dieguitosus@hotmail.com>
2021-07-02 20:16:40 -0300

    added 5

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+def island_perimeter(grid):
+    perimetro = 0
+    for fila in range(0, len(grid)):
+        for col in range(0, len(grid[fila])):
+            if grid[fila][col] == 1:
+                if col < len(grid[fila]) - 1 and grid[fila][col + 1] == 0:
+                    perimetro += 1
+                if col > 0 and grid[fila][col - 1] == 0:
+                    perimetro += 1
+                if fila > 0 and grid[fila - 1][col] == 0:
+                    perimetro += 1
+                if fila < len(grid) - 1 and grid[fila + 1][col] == 0:
+                    perimetro += 1
+                if fila == 0 or fila == len(grid) - 1:
+                    perimetro += 1
+                if col == 0 or col == len(grid[fila]) - 1:
+                    perimetro += 1
+    return perimetro
