19ca329c7eb0ff291bbe571f83f0ab1d7966b3f6
DiegoSusviela <dieguitosus@hotmail.com>
2021-09-01 22:18:18 -0300

    added interpolation

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -11,5 +11,25 @@
 
 int interpolation_search(int *array, size_t size, int value)
 {
+	size_t low = 0, pos, high = size - 1;
 
+	if (!array)
+		return (-1);
+	while (array[low] != array[high])
+	{
+		pos = low + (((double)(high - low) / (array[high] - array[low])) * (value - array[low]));
+		if (pos < low || pos > high)
+		{
+			printf("Value checked array[%lu] is out of range\n", pos);
+			break;
+		}
+		printf("Value checked array[%lu] = [%d]\n", pos, array[pos]);
+		if (array[pos] < value)
+			low = pos + 1;
+		else 
+			if (array[pos] > value)
+				high = pos - 1;
+		else
+			return (pos);
+	}
 }
